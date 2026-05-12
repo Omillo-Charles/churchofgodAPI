5bf12beb74b96a3ca63e69c036092faa2938d68b
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-12 18:31:00 +0300

    refactor: rename auth route file, update database connection environment variable, and defer connection to server startup

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import { connectDB } from "./database/Neon.js";
-import authRouter from "./routes/auth.route.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use("/api/v1/auth", authRouter);
-
-// Connect to Database
-connectDB();
 
 app.get("/", (req, res) => {
     res.json({
@@ -22,8 +18,9 @@ app.get("/", (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`The NTCOGK Backend API is running on http://localhost:${PORT}`)
+    await connectDB();
 })
 
 export default app;
